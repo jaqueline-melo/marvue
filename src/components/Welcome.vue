c2c99bac39c7e1ba03b4609975a8c8c51c3e1e1f
marco-tulio-melo <marcotulio2120@gmail.com>
2020-02-06 13:16:08 -0300

    Pattern for field masks

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,24 +1,31 @@
 <template>
-
-  <v-container  >
-    <v-layout text-center wrap>
-
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
       <v-flex xs12>
-        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
       </v-flex>
 
       <v-flex mb-4>
         <h1 class="display-1 font-weight-bold mb-3 white--text">
           Best boilerplate VueJS
         </h1>
-
       </v-flex>
-  
+
       <v-flex
         mb-5
         xs12
       >
-        <h2 class="headline font-weight-bold mb-3 white--text">What's next?</h2>
+        <h2 class="headline font-weight-bold mb-3 white--text">
+          What's next?
+        </h2>
 
         <v-layout justify-center>
           <a
@@ -27,15 +34,17 @@
             :href="next.href"
             class="subheading mx-3 white--text"
             target="_blank"
-          >
-            {{ next.text }}
-          </a>
+          >{{ next.text }}</a>
         </v-layout>
       </v-flex>
 
-      <v-flex xs12 mb-5>
-
-        <h2 class="headline font-weight-bold mb-3 white--text">Ecosystem</h2>
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3 white--text">
+          Ecosystem
+        </h2>
 
         <v-layout justify-center>
           <a
@@ -44,90 +53,118 @@
             :href="eco.href"
             class="subheading mx-3 white--text"
             target="_blank"
-          >
-            {{ eco.text }}
-          </a>
+          >{{ eco.text }}</a>
         </v-layout>
-      </v-flex >
-       <v-flex xs12 mb-5 justify-center>
-          <h2 class="headline font-weight-bold mb-3 white--text">Graphic</h2>
+      </v-flex>
+      <v-flex
+        xs12
+        mb-5
+        justify-center
+      >
+        <h2 class="headline font-weight-bold mb-3 white--text">
+          Graphic
+        </h2>
         <v-layout justify-center>
-          <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
-       </v-layout>
+          <apexchart
+            width="500"
+            type="bar"
+            :options="options"
+            :series="series"
+          />
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+        justify-center
+      >
+        <h2 class="headline font-weight-bold mb-3 white--text">
+          vue-the-mask
+        </h2>
+        <v-layout justify-center>
+          <div class="white">
+            <input
+              v-mask="['###.###.###-##', '##.###.###/####-##']"
+              name="cpf/cnpj"
+              placeholder="CPF/CNPJ"
+            />
+          </div>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
-
 </template>
 
 <script>
-
 export default {
   name: 'HelloWorld',
 
   data: () => ({
-     options: {
-        chart: {
-          id: 'vuechart-example'
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        }
+    options: {
+      chart: {
+        id: 'vuechart-example'
       },
-      series: [{
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+      }
+    },
+    series: [
+      {
         name: 'series-1',
         data: [30, 40, 45, 50, 49, 60, 70, 91]
-      }],
+      }
+    ],
     ecosystem: [
       {
         text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
       },
       {
         text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
+        href: 'https://github.com/vuetifyjs/vuetify'
       },
       {
         text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
     ],
     importantLinks: [
       {
         text: 'Documentation',
-        href: 'https://vuetifyjs.com',
+        href: 'https://vuetifyjs.com'
       },
       {
         text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
+        href: 'https://community.vuetifyjs.com'
       },
       {
         text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
+        href: 'https://madewithvuejs.com/vuetify'
       },
       {
         text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
+        href: 'https://twitter.com/vuetifyjs'
       },
       {
         text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
+        href: 'https://medium.com/vuetify'
+      }
     ],
     whatsNext: [
       {
         text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
+        href: 'https://vuetifyjs.com/components/api-explorer'
       },
       {
         text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
+        href: 'https://vuetifyjs.com/layout/pre-defined'
       },
       {
         text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
-};
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+      }
+    ]
+  })
+}
 </script>
